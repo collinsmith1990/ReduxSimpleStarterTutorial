a22749683951d1c0c5ff94911c2d8b1ddec2f222
CollinSmith <collinsmith1990@gmail.com>
2016-02-14 13:07:16 -0700

    Added search and select functionality.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,12 +9,17 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input 
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.handleInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  handleInputChange(term) {
+    this.setState({term});
+    this.props.handleSearchTermChange(term);
   }
 }
 
