4ba62bbd40ed12b2e5d994e6abab030377d4c7d4
CollinSmith <collinsmith1990@gmail.com>
2016-02-14 03:51:17 -0700

    Added video detail component.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyD_6kTicuVZGqzy9ycqlAJMEqBoWVgtNNM';
 
-const API_KEY = 'not-a-secret';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Create a new component. This component should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-//Take this compnent's generated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
