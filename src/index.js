d1d09f39dad62e5ed9490e91458708a9b781211c
CollinSmith <collinsmith1990@gmail.com>
2016-02-13 21:59:40 -0700

    Added state to search_bar component.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'not-a-secret';
+
 // Create a new component. This component should produce HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //Take this compnent's generated HTML and put it in the DOM
